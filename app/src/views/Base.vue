175aa65a54232f66ba9dc889a001bb5c825d87f4
Your Name <ivang112@nycstudents.net>
2026-03-02 11:49:18 -0500

    s

--- app/src/views/Base.vue
+++ app/src/views/Base.vue
@@ -1,13 +1,39 @@
 <template>
     <div>
-        <h2>Make a Pizza!</h2>
-        <h3>Get Started</h3>
+        <h1><router-link to="/pizza">Make a Pizza!</router-link></h1>
+        <h2>Get Started</h2>
     </div>
 </template>
 
 <script>
+import { useRouter } from 'vue-router';
+const router = useRouter();
+const gotoToppings = () => {
+    router.push('/toppinglist');
+}
 </script>
 
 <style scoped>
-
+:global(body){    
+    background-color: #f88379;
+}
+div{
+    text-align: center;
+    justify-content: center;
+    padding: 40px;
+    display: flex;
+    flex-direction: column;
+    margin:auto;
+    max-width: fit-content;
+}
+h2{
+    font-size: 25px;
+    border: #f29c9c solid 4px;
+    border-radius: 7.5px;
+}
+h1{
+    font-size: 40px;
+    border: #f29c9c solid 4px;
+    border-radius: 10px;
+}
 </style>
