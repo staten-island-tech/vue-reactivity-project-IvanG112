1983db2d6530c6dde93c45f4a7312a20920e85f3
Your Name <ivang112@nycstudents.net>
2026-03-04 12:29:59 -0500

    ;

--- app/src/views/ToppingList.vue
+++ app/src/views/ToppingList.vue
@@ -1,32 +1,33 @@
 <template>
-<div class="pizza">
-    <div class="base">
-        <img src="/pizzabase.png" alt="Pizza">
+    <div class="pizza">
+        <div class="base">
+            <img src="/pizzabase.png" alt="PizzaBase">
+            <img src="/sauceandcheese.png" alt="PizzaSauceandCheese">
+        </div>
+        <div class="toppingList">
+            <Toppings v-for="topping in toppinglist" :key="topping.name" :topping="topping">
+                <button>Add To Pizza</button>
+            </Toppings>
+        </div>
     </div>
-    <div class="toppingList">
-        <Toppings v-for="topping in toppinglist" :key="topping.name" :topping="topping">
-            <button>Add To Pizza</button>
-        </Toppings>
-    </div>
-</div>
-    
+
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { reactive } from 'vue';
 import Toppings from '@/components/Toppings.vue';
-const toppinglist = ref([
-    {name: 'Pepperoni',type: 'meat',price: 2.50},
-    {name: 'Italian Sausage',type: 'meat',price: 2.50},
-    {name: 'BBQ Chicken',type: 'meat',price: 2.50},
-    {name: 'Buffalo Chicken',type: 'meat',price: 2.50},
-    {name: 'Bacon',type: 'meat',price: 2.50},
-    {name: 'Ham',type: 'meat',price: 2.50},
-    {name: 'Mushrooms',type: 'vegetable',price: 1.50},
-    {name: 'Onions',type: 'vegetable',price: 1.50},
-    {name: "Bell Peppers",type: 'vegetable',price: 1.50},
-    {name: 'Olives',type: 'vegetable',price: 1.50},
-    {name: 'Jalapenos',type: 'vegetable',price: 1.50},
+const toppinglist = reactive([
+    { name: 'Pepperoni', type: 'meat', price: 2.50 },
+    { name: 'Italian Sausage', type: 'meat', price: 2.50 },
+    { name: 'BBQ Chicken', type: 'meat', price: 2.50 },
+    { name: 'Buffalo Chicken', type: 'meat', price: 2.50 },
+    { name: 'Bacon', type: 'meat', price: 2.50 },
+    { name: 'Ham', type: 'meat', price: 2.50 },
+    { name: 'Mushrooms', type: 'vegetable', price: 1.50 },
+    { name: 'Onions', type: 'vegetable', price: 1.50 },
+    { name: "Bell Peppers", type: 'vegetable', price: 1.50 },
+    { name: 'Olives', type: 'vegetable', price: 1.50 },
+    { name: 'Jalapenos', type: 'vegetable', price: 1.50 },
 ])
 
 </script>
@@ -36,17 +37,21 @@ const toppinglist = ref([
     display: flex;
     flex-wrap: wrap;
     width: 60%;
-    justify-content: flex-end;    
-    
+    position: relative;
+    left: 400px;
+
 }
+
 button {
     margin-top: 10px;
     margin-bottom: 20px;
     padding: 5px;
     width: 100px;
 }
+
 img {
-    width: 400px;
-    height: 400px;
+    width: 250px;
+    height: 250px;
+    position: absolute;
 }
 </style>
